774d3d7c123b6c179147f30c2ca70b52339bf431
Brian Barnes <barnesb7@gmail.com>
2017-04-13 13:44:06 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var temp = 70;
+
+var converted = ((temp -32)* 5)/9;
+
+console.log('The temperature ' + temp +
+          " degrees Fahrenheit is equal to "
+          + converted + " degrees Celcius.")
+
+var celciusTemp = 21.111;
+
+var converted2 = ((celciusTemp * 9)/5) + 32;
+
+
+console.log('The temperature ' + celciusTemp +
+          " degrees Celcius is equal to "
+          + converted2 + " degrees fahrenheit.")
